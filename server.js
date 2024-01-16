98ae0329d27883e2161016a282de1a0be1eaba5d
BoraKucukkara <89361281+BoraKucukkara@users.noreply.github.com>
2024-01-16 04:33:11 +0300

    db connection

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(()=>console.log("Connected to DB/followers"))
+.catch((err)=> console.error(err))
+
+
+app.listen(5001, () => console.log("▶️ Server Started"))
