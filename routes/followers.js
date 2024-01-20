7a55b57515a5c9be07ed6be982a91b1b080fafc3
BoraKucukkara <89361281+BoraKucukkara@users.noreply.github.com>
2024-01-20 17:47:15 +0300

    logger func

--- routes/followers.js
+++ routes/followers.js
@@ -3,37 +3,35 @@ const router = express.Router()
 const Followers = require('../models/followers');
 
 // Getting all records
-router.get('/', async (req, res)=>{
-    console.log("request: GET all records")
+router.get('/',async (req, res)=>{
     try {
         const followers = await Followers.find()
         res.json(followers)
-        console.log("request: GET all records: SUCCESS")
     } catch (err) {
         res.status(500).json({message: err.message})
     }
+    eventLog(req,res)
 })
 
 // Creating record
 router.post('/', async (req, res)=>{
-    console.log("request: POST create new record")
     const follower = new Followers({
         name: req.body.name,
         followed: req.body.followed,
     })
     try {
         const newFollower = await follower.save()
-        res.status(201).json(newFollower)
-        console.log("request: POST create new record: SUCCESS")
+        res.status(201).json(newFollower) 
     } catch(err) {
         res.status(400).json({message: err.message})
     }
+    eventLog(req, res)
 })
 
 // Getting one record
 router.get('/:id', getFollower, (req, res)=>{
     res.json(res.follower)
-    console.log("request: GET by id:" + res.follower.id  + " SUCCESS")
+    eventLog(req, res)
 })
 
 // Update record
@@ -47,10 +45,10 @@ router.patch('/:id', getFollower, async (req, res)=>{
     try {
         const updatedFollower = await res.follower.save()
         res.json(updatedFollower)
-        console.log("request: PATCH by id:" + res.follower.id + " SUCCESS") 
     } catch (error) {
         res.status(400).json({message: error.message})
     }
+    eventLog(req, res)
 })
 
 // Delete record
@@ -58,15 +56,23 @@ router.delete('/:id', getFollower, async (req, res)=>{
     try {
         await res.follower.deleteOne()
         res.json({message: res.follower.id + " record deleted"})
-        console.log("request: DELETE id:" + res.follower.id + " SUCCESS")
     } catch (err) {
         res.status(500).json({message: err.message})
     }
+    eventLog(req, res)
 })
+
+// event Logger
+function eventLog(req, res) {
+    let date = new Date()
+    console.log(`
+┌ ${date.toTimeString()}
+├ request____:${req.method} ${req.baseUrl} ${req.url}
+└ response___:${res.statusCode} ${res.statusMessage}`)
+}
 
 // Middleware
 async function getFollower(req, res, next) {
-    console.log("Incoming request by ID")
     let follower
     try {
         follower = await Followers.findById(req.params.id)
@@ -82,7 +88,6 @@ async function getFollower(req, res, next) {
     res.follower = follower
     next()
 }
-
 
 module.exports = router
 
